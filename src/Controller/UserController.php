a3ae77636744b20f8b448aaffc962ab240f8bcce
RumpelRaisch <rainer@bitshifting.de>
2018-10-21 00:12:56 +0200

    adjusted login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,11 +13,44 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class UserController extends Abstracts\AbstractController
 {
+    public const CONTROLLER_NAME = 'user';
+    public const SESSION_USER    = self::SESSION_ROOT . '/' . self::CONTROLLER_NAME;
+
+    private $context = [];
+
+    /**
+     * Constructor.
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        parent::__construct();
+
+        $this
+            ->setDefaultSession()
+            ->setDefaultLogger($kernel);
+
+        $this->context['__AREA__'] = 'UserController';
+
+        if (null === $this->getSession()->get(self::SESSION_USER, null)) {
+            $this->getSession()->set(self::SESSION_USER, []);
+        }
+    }
+
     /**
      * @Route("/login", name="user.login")
      */
-    public function loginView()
+    public function loginView(): Response
     {
-        return $this->render('user/login.html.twig', []);
+        $this->getLogger()->trace(self::CONTROLLER_NAME . '.login', $this->context);
+
+        return $this->render(self::CONTROLLER_NAME . '/login.html.twig', [
+            'config' => [
+                'pageTitle'      => ucfirst(self::CONTROLLER_NAME) . ' Login',
+                'contentClasses' => 'd-flex align-items-center',
+                'showNavBar'     => false,
+                'showSideBar'    => false,
+                'showFooter'     => false,
+            ] + $this->getBaseTemplateConfig(),
+        ]);
     }
 }
