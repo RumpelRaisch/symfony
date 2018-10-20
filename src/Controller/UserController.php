e05674bcea1e086d122d8d324177b092e8ec7568
RumpelRaisch <rainer@bitshifting.de>
2018-10-20 09:01:31 +0200

    started working on user related stuff

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * [UserController description]
+ *
+ * @author Rainer Schulz <rainer.schulz@bitshifting.de>
+ */
+class UserController extends Abstracts\AbstractController
+{
+    /**
+     * @Route("/login", name="user.login")
+     */
+    public function index()
+    {
+        return $this->render('user/login.html.twig', []);
+    }
+}
