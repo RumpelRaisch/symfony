637b2d2304baf581496e83395a1f711ca8e373f1
RumpelRaisch <rainer@bitshifting.de>
2018-10-31 23:06:42 +0100

    updated flag interface

--- src/Flag/Interfaces/FlagInterface.php
+++ src/Flag/Interfaces/FlagInterface.php
@@ -4,35 +4,55 @@ namespace App\Flag\Interfaces;
 interface FlagInterface
 {
     /**
-     * @param int $flag
+     * @param int    $flag
+     * @param string $name
      *
      * @return self
      */
-    public function setFlag(int $flag);
+    public function setFlag(int $flag, string $name = 'default');
 
     /**
-     * @param int $flag
+     * @param int    $flag
+     * @param string $name
      *
      * @return self
      */
-    public function removeFlag(?int $flag);
+    public function removeFlag(?int $flag, string $name = 'default');
 
     /**
-     * @param int $flag
+     * @param int    $flag
+     * @param string $name
      *
      * @return bool
      */
-    public function issetFlag(?int $flag): bool;
+    public function issetFlag(?int $flag, string $name = 'default'): bool;
 
     /**
+     * @param string $name
+     *
      * @return int current flags
      */
-    public function getFlags(): ?int;
+    public function getFlags(string $name = 'default'): ?int;
 
     /**
-     * @param int $flags
+     * @param int    $flags
+     * @param string $name
      *
      * @return self
      */
-    public function setFlags(int $flags);
+    public function setFlags(int $flags, string $name = 'default');
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function issetFlags(string $name = 'default'): bool;
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function unsetFlags(string $name = 'default');
 }
